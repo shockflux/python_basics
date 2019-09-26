5857c85d7575798fe96e55a1badbaefdb83d71ff
shockflux <sbswaroop69@gmail.com>
2019-09-26 19:46:52 +0530

    first commit

---
+++ basic1.py
@@ -0,0 +1,9 @@
+#students details
+
+id=int(input("enter student id:"))
+name=input("enter student name")
+marks=float(input("enter student marks"))
+
+print("student name is",name)
+print("student id is ",id)
+print("student mark is",marks)
